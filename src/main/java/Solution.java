397c9fd3fe29fa251fef3c8a63182593ef4c19af
LisaH <lisahongofficial@gmail.com>
2023-10-10 22:19:49 -0400

    ch8. add AllCharsInName method(FlatMap)

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,5 +1,8 @@
+import java.sql.SQLOutput;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class Solution {
@@ -12,12 +15,19 @@ public class Solution {
         System.out.println();
         System.out.println("First three people on the list");
         firstThreePeople().forEach(System.out::println);
+        System.out.println();
+        System.out.println("List of people on the list skipped first two people");
+        skipTwoPerson().forEach(System.out::println);
+        System.out.println();
+        displayNames();
+        System.out.println();
+        System.out.println(AllCharsInNames());
 
     }
 
-//   1. **Filtering (Intermediate Operation):**
+    //   1. **Filtering (Intermediate Operation):**
 //       - Filter the list of persons to include only males.
-    static List<String> malesOnly(List<Person> people){
+    static List<String> malesOnly(List<Person> people) {
         people = Person.persons();
         List<String> males = people.stream()
                 .filter(Person::isMale)
@@ -26,9 +36,9 @@ public class Solution {
         return males;
     }
 
-//    2. **Mapping (Intermediate Operation):**
+    //    2. **Mapping (Intermediate Operation):**
 //            - Map the list of persons to their names.
-    static List<String> namesOnly(List<Person> people){
+    static List<String> namesOnly(List<Person> people) {
         people = Person.persons();
         List<String> names = people.stream()
                 .map(Person::getName)
@@ -36,21 +46,21 @@ public class Solution {
         return names;
     }
 
-//    3. **Sorting (Intermediate Operation):**
+    //    3. **Sorting (Intermediate Operation):**
 //            - Sort the list of persons by their income in descending order.
-    static List<Person> sortedByIncomeDesc(){
+    static List<Person> sortedByIncomeDesc() {
         List<Person> sortedList = Person.persons()
                 .stream()
                 .sorted(Comparator.comparing(Person::getIncome).reversed())
                 .toList();
-//                .sorted(Comparator.comparing(Person::getIncome).thenComparing(Person::getName)
+//                .sorted(Comparator.comparing(Person::getIncome).thenComparing(Person::getName))
 //                .toList();
         return sortedList;
     }
 
-//    4. **Distinct (Intermediate Operation):**
+    //    4. **Distinct (Intermediate Operation):**
 //            - Find the distinct genders in the list of persons.
-    static List<Person.Gender> distinctGender(){
+    static List<Person.Gender> distinctGender() {
         List<Person.Gender> genders = Person.persons()
                 .stream()
                 .map(Person::getGender)
@@ -59,9 +69,9 @@ public class Solution {
         return genders;
     }
 
-//    5. **Limit (Intermediate Operation):**
+    //    5. **Limit (Intermediate Operation):**
 //            - Limit the list of persons to the first 3.
-    static List<Person> firstThreePeople(){
+    static List<Person> firstThreePeople() {
         List<Person> top3 = Person.persons()
                 .stream()
                 .limit(3)
@@ -69,5 +79,37 @@ public class Solution {
 
         return top3;
     }
+
+    //    6. **Skip (Intermediate Operation):**
+//            - Skip the first 2 persons in the list.
+    static List<Person> skipTwoPerson() {
+        List<Person> skippedPeople = Person.persons()
+                .stream()
+                .skip(2)
+                .toList();
+        return skippedPeople;
+    }
+
+    //7. **Peek (Intermediate Operation):**
+//            - Use peek to print the names of all persons in the list.
+    static void displayNames() {
+        Person.persons()
+                .stream()
+                .peek(p -> System.out.println("Person name" + p.getName()))
+                .forEach(System.out::println);
+
+    }
+//
+//    8. **FlatMap (Intermediate Operation):**
+//            - Map each person to a stream of characters representing their names.
+    static List<Character> AllCharsInNames(){
+        return Person.persons()
+                .stream()
+                .flatMap(p -> p.getName().chars().mapToObj(c -> (char)c))
+                .collect(Collectors.toList());
+    }
+
+
+
 
 }
